2c2c9ca0e5ebec9194351ddb15b998956947b227
Januar <adhinugroho623@gmail.com>
2024-03-25 19:13:50 +0700

    update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="inline-flex gap-2">
     <NuxtLink
-      to="mailto:kelvingeraldi21@gmail.com"
-      aria-label="Linktree"
+      to=""
+      aria-label="Tiktok"
       target="_blank"
       :external="true"
       class="rounded-md transition-colors duration-300 w-8 h-8 hover:bg-black/5 hover:dark:bg-white/5 flex items-center"
     >
-      <Icon name="material-symbols:mail-rounded" class="w-full h-full p-1" />
+      <Icon name="material-symbols:tiktok-rounded" class="w-full h-full p-1" />
     </NuxtLink>
     <NuxtLink
-      to="https://twitter.com/crstlnz"
-      aria-label="@Crstlnz"
+      to="https://twitter.com/januaradhin"
+      aria-label="@januaradhin"
       target="_blank"
       :external="true"
       class="rounded-md transition-colors duration-300 w-8 h-8 hover:bg-black/5 hover:dark:bg-white/5 flex items-center"
@@ -19,7 +19,7 @@
       <Icon name="ri:twitter-x-line" class="w-full h-full p-1.5" />
     </NuxtLink>
     <NuxtLink
-      to="https://github.com/crstlnz"
+      to="https://github.com/januaradhiN77"
       aria-label="Github"
       target="_blank"
       :external="true"
